7ac149918a49c958c631b9eeb8ba4c341663985b
Raymond de Hooge <Raymond.dehooge@gmail.com>
2020-11-02 13:09:41 +0100

    Added lock and lights controls

--- box3dFanCtrl/__init__.py
+++ box3dFanCtrl/__init__.py
@@ -224,14 +224,14 @@ class Box3dfanctrlPlugin(octoprint.plugin.BlueprintPlugin,
 		# self.pi.write(self.pin("lock"),pigpio.LOW)
 
 	@octoprint.plugin.BlueprintPlugin.route('/unlock', methods=["POST"])
-	def set_unlock(self):
+	def unlock(self):
 		temp = self.to_int(request.values["temperature"])
 		self._logger.info("temp val(for lock)= %d" % temp)
 		self.set_unlock()
 		return jsonify(success=True)
 		
 	@octoprint.plugin.BlueprintPlugin.route('/lock', methods=["POST"])
-	def set_lock(self):
+	def lock(self):
 		self.set_lock()
 		return jsonify(success=True)
 
